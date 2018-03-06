fbef97cb82f32d348910c67ae4561ba301a4618d
EricJSutton33 <EricJSutton33@gmail.com>
2018-03-06 11:19:40 -0800

    Added Sensor Value Placeholders
    In the file 'General', added string variables for each sensor

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*__________________________________
+
+              P R O G R A M   C L A S S
+              
+   For XBEE WeatherStation Telemetry Demo
+                   Created 8/18/2017
+                         Ron Kessler
+____________________________________
+
+ This class is the entry point for the app.
+
+ */
+
+
+using System;
+using System.Windows.Forms;
+
+
+namespace XBEE_READER
+{
+    static class Program
+    {
+       
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());                  //now create mainForm object
+        }
+    }
+}
