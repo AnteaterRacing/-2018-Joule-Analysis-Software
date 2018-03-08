98b03025224ea7b3f8834211757fefb4fce716fc
EricJSutton33 <EricJSutton33@gmail.com>
2018-03-08 10:50:54 -0800

    Revert "Added Sensor Value Placeholders"
    This reverts commit fbef97cb82f32d348910c67ae4561ba301a4618d.

--- WindowsFormsApp1/MIDDLE TIER/General.cs
+++ WindowsFormsApp1/MIDDLE TIER/General.cs
@@ -59,68 +59,14 @@ namespace XBEE_READER
         internal static bool DoFileLogging { get; set; }
         internal static bool Log2SQL { get; set; }
         internal static string MSGTITLE = "UCI Race Car Telemetry";
-
+       
 
         //---sensor data
         internal static string XAxis { get; set; }
         internal static string YAxis { get; set; }
         internal static string ZAxis { get; set; }
 
-
-        // Tire temperature sensor data
-        internal static string TTBR1 { get; set; }
-        internal static string TTBR2 { get; set; }
-        internal static string TTBR3 { get; set; }
-
-        internal static string TTBL1 { get; set; }
-        internal static string TTBL2 { get; set; }
-        internal static string TTBL3 { get; set; }
-
-        internal static string TTFR1 { get; set; }
-        internal static string TTFR2 { get; set; }
-        internal static string TTFR3 { get; set; }
-
-        internal static string TTFL1 { get; set; }
-        internal static string TTFL2 { get; set; }
-        internal static string TTFL3 { get; set; }
-
-
-        // Motor temperature sensor data
-        internal static string MT1 { get; set; }
-        internal static string MT2 { get; set; }
-
-
-        // Wheel speed sensor data
-        internal static string WSBR { get; set; }
-        internal static string WSBL { get; set; }
-        internal static string WSFR { get; set; }
-        internal static string WSFL { get; set; }
-
-
-        // Throttle position sensor data
-        internal static string throttleR { get; set; }
-        internal static string throttleL { get; set; }
-
-
-        // Battery pack voltage, current, and temperature sensor data
-        internal static string packVoltage { get; set; }
-        internal static string packCurrent { get; set; }
-        internal static string packTemperature { get; set; }
-
-
-        // Steering angle sensor data
-        internal static string steeringAngle { get; set; }
-
-
-        // Accelerator and brake angle
-        internal static string accelAngle { get; set; }
-        internal static string brakeAngle { get; set; }
-
-
-
-
-        internal static string receiveString { get; set; }
-
+        internal static byte testString { get; set; }
 
 
         #endregion
